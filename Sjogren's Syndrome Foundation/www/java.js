fecf4ce38230121165791178206ac16c1407a2b2
Theundyingcode <gdwatt@knox.edu>
2018-02-01 23:53:31 -0600

    finish init_index

--- Sjogren's Syndrome Foundation/www/java.js
+++ Sjogren's Syndrome Foundation/www/java.js
@@ -2,21 +2,36 @@ function go_back() {
     window.history.back();
 }
 
-function read_csv(file) {
-    var lines = file.split(/\r\n|\n/);
+function store_csv(data, type) {
+    var lines = data.split(/\r\n|\n/);
     for (i = 0; i < lines.length; i++) {
-        lines[i] = lines[i].split(',');
+        lines[i] = lines[i].split(',',1);
+        sessionStorage.setItem( concat(type, id), lines[i] );
     }
-    return lines;
 }
 
-function init_index(){
-    
+function init_index(types){
+    (function(){
+        var xhr = [];
+        for (i = 0; i < 3; i++){
+            (function (i){
+                xhr[i] = new XMLHttpRequest();
+                url = types[i] + "_doc/" + types[i] + "_index.csv";
+                xhr[i].open("GET", url, true);
+                xhr[i].onreadystatechange = function () {
+                    if (xhr[i].readyState == 4 && xhr[i].status == 200) {
+                        store_csv(xhr[i].responseText, types[i]);
+                    }
+                };
+                xhr[i].send();
+            })(i);
+        }
+    })();
 }
 
 function goto_page(id, type) {
-    localStorage.page_type = type;
-    localStorage.page_id = id;
+    sessionStorage.page_type = type;
+    sessionStorage.page_id = id;
     window.location.assign(id + "_page.html");
 }
 
@@ -33,5 +48,5 @@ function goto_survival(id) {
 }
 
 function get_content(id, type) {
-
+    
 }
